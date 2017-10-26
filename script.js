dff48b84fe2ec3a0b1399f30a0d6fcc2bd4c1a03
Guilherme Soldateli <guilherm3soldateli@gmail.com>
2017-10-26 12:31:57 +0000

    Finished project.

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-var scrollEvent;
+var scrollEvent,
+	scrollPageEvent;
+
+function scrollPageTo(selector) {
+	var yPos = document.querySelector(selector).offsetTop;
+	window.scroll(0,yPos - 80);	
+}
 
 function scrollX(x) {
 	document.querySelector('.project-list').scrollLeft += x ;	
@@ -6,13 +12,13 @@ function scrollX(x) {
 
 document.querySelector('.project-list-arrow.left').addEventListener('mouseover',function(){
 	scrollEvent = setInterval(function(){
-		scrollX(3);}
+		scrollX(-3);}
 		,10);
 });
 
 document.querySelector('.project-list-arrow.right').addEventListener('mouseover',function(){
 	scrollEvent = setInterval(function(){
-		scrollX(-3);}
+		scrollX(3);}
 		,10);
 });
 
@@ -23,4 +29,9 @@ document.querySelector('.project-list-arrow.left').addEventListener('mouseout',f
 document.querySelector('.project-list-arrow.right').addEventListener('mouseout',function(){
 	clearInterval(scrollEvent);
 });
+
+function scrollPageTo(selector) {
+	var yPos = document.querySelector(selector).offsetTop;
+	window.scroll(0,yPos - 80);	
+}
 
