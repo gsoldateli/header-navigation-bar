18c5eb434c6a790b0b3433f4bf3c8a912e58fa01
Guilherme Soldateli <guilherm3soldateli@gmail.com>
2017-12-02 12:45:18 +0000

    Wrapped JS code inside a IFEE.

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
-var scrollEvent,
-	scrollPageEvent;
+(function(window){
+	var scrollEvent,
+		scrollPageEvent;
 
-function scrollPageTo(selector) {
-	var yPos = document.querySelector(selector).offsetTop;
-	window.scroll(0,yPos - 80);	
-}
+	function scrollX(x) {
+		document.querySelector('.project-list').scrollLeft += x ;	
+	}
 
-function scrollX(x) {
-	document.querySelector('.project-list').scrollLeft += x ;	
-}
+	document.querySelector('.project-list-arrow.left').addEventListener('mouseover',function(){
+		scrollEvent = setInterval(function(){
+			scrollX(-3);}
+			,10);
+	});
 
-document.querySelector('.project-list-arrow.left').addEventListener('mouseover',function(){
-	scrollEvent = setInterval(function(){
-		scrollX(-3);}
-		,10);
-});
+	document.querySelector('.project-list-arrow.right').addEventListener('mouseover',function(){
+		scrollEvent = setInterval(function(){
+			scrollX(3);}
+			,10);
+	});
 
-document.querySelector('.project-list-arrow.right').addEventListener('mouseover',function(){
-	scrollEvent = setInterval(function(){
-		scrollX(3);}
-		,10);
-});
+	document.querySelector('.project-list-arrow.left').addEventListener('mouseout',function(){
+		clearInterval(scrollEvent);
+	});
 
-document.querySelector('.project-list-arrow.left').addEventListener('mouseout',function(){
-	clearInterval(scrollEvent);
-});
+	document.querySelector('.project-list-arrow.right').addEventListener('mouseout',function(){
+		clearInterval(scrollEvent);
+	});
 
-document.querySelector('.project-list-arrow.right').addEventListener('mouseout',function(){
-	clearInterval(scrollEvent);
-});
 
-function scrollPageTo(selector) {
-	var yPos = document.querySelector(selector).offsetTop;
-	window.scroll(0,yPos - 80);	
-}
+	window.scrollPageTo = function(selector) {
+		var yPos = document.querySelector(selector).offsetTop;
+		window.scroll(0,yPos - 80);	
+	}
+})(window);
 
